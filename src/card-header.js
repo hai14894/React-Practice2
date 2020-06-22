4d37eeea9ef766d5baea579777919b544979356f
hai14894 <hohoanghai14894@gmail.com>
2020-06-23 09:48:27 +1000

    add card header

---
+++ src/card-header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MetaData from './metadata'
+const CardHeader = () => {
+    return (
+            <div class="card-header">
+                <h1 class="title">Space Force</h1>
+                <MetaData/>
+            </div>
+    )
+}
+export default CardHeader
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MetaData from './metadata'
+import CardHeader from './card-header'
 
 const Page = () =>{
     return (
         <>
-            <MetaData/>
+            <CardHeader/>
         </>
     )
 }
