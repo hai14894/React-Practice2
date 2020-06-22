bb727f326992ae29889c4325c5a413983b8dea5f
hai14894 <hohoanghai14894@gmail.com>
2020-06-23 09:58:24 +1000

    add card btn

---
+++ src/card-btn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const CardBtn = () => {
+    return (
+            <div className="card-buttons">
+                <button>Play</button>
+                <button>+ My List</button>
+                <div class="icon">+1</div>
+                <div class="icon">-1</div>
+            </div>
+    )
+}
+export default CardBtn
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom"
 import CardHeader from './card-header'
 import CardDescription from "./card-description"
+import CardBtn from "./card-btn"
 
 const Page = () =>{
     return (
         <>
             <CardHeader/>
             <CardDescription/>
+            <CardBtn/>
         </>
     )
 }
